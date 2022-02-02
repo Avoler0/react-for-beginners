fb085b20fe0b77d884449fa0ecdbd2abeded176a
Avoler0 <jbu1391@gmail.com>
2022-02-03 04:10:45 +0900

    Add build Folder

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ import {
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 function App() {
-  return (<Router>
+  return (<Router basename={process.env.PUBLIC_URL}>
     <Routes>
-      Route path={`${process.env.PUBLIC_URL}/`} element={Home}
+      <Route path="/" element={Home} />
       <Route path="/movie/:id" element={<Detail/>} />
     </Routes>
   </Router>
